7b0350a800e9025475a359a664376a25b75aa121
younesse <younesse.amzil@gmail.com>
2026-01-23 15:00:48 +0100

    best seller list view tem

--- lib/features/Home/presentation/view/widget/home_view_body.dart
+++ lib/features/Home/presentation/view/widget/home_view_body.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:my_bookly/Core/Utils/Styles.dart';
+import 'package:my_bookly/Core/Utils/assets.dart';
 import 'package:my_bookly/features/Home/presentation/view/widget/app_bar_home.dart';
 import 'package:my_bookly/features/Home/presentation/view/widget/featured_list_view.dart';
 
@@ -17,13 +18,40 @@ class HomeViewBody extends StatelessWidget {
           CustoumAppBar(),
           FeaturedListView(),
           SizedBox(height: 45),
-          Text(
-            'Best Seller',
-            style: Stayles.titleMediem,
-       
-          ),
+          Text('Best Seller', style: Stayles.titleMediem),
+          SizedBox(height: 25),
+          BestSellerListViewItem(),
         ],
       ),
+    );
+  }
+}
+
+class BestSellerListViewItem extends StatelessWidget {
+  const BestSellerListViewItem({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        SizedBox(
+          height: 130,
+          child: AspectRatio(
+            aspectRatio: 2.7 / 4,
+            child: Container(
+              decoration: BoxDecoration(
+                color: Colors.red,
+                borderRadius: BorderRadius.circular(10),
+                image: DecorationImage(
+                  fit: BoxFit.fill,
+                  image: AssetImage(AssetsData.testimage),
+                ),
+              ),
+            ),
+          ),
+        ),
+        Column(children: []),
+      ],
     );
   }
 }
