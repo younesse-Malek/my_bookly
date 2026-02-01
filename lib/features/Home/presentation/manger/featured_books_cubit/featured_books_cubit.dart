d6528726d6afeb11fd6c2449b0a552dd3cc2032b
younesse <younesse.amzil@gmail.com>
2026-02-01 23:57:09 +0100

    craete featured books method

--- lib/features/Home/presentation/manger/featured_books_cubit/featured_books_cubit.dart
+++ lib/features/Home/presentation/manger/featured_books_cubit/featured_books_cubit.dart
@@ -1,9 +1,24 @@
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
 import 'package:my_bookly/features/Home/data/models/book_moudel/book_moudel.dart';
+import 'package:my_bookly/features/Home/data/repo/hom_repo_implement.dart';
 
 part 'featured_books_state.dart';
 
 class FeaturedBooksCubit extends Cubit<FeaturedBooksState> {
-  FeaturedBooksCubit() : super(FeaturedBooksInitial());
+  FeaturedBooksCubit(this.homeRepo) : super(FeaturedBooksInitial());
+  final HomRepoImpl homeRepo;
+
+  Future<void> featchFeaturedbooks() async {
+    emit(FeaturedBooksLodaing());
+    var resulte = await homeRepo.fetchFeatureBooks();
+    resulte.fold(
+      (feailure) {
+        emit(FeaturedBooksFailure(errMessage: feailure.message));
+      },
+      (books) {
+        emit(FeaturedBooksSucces(listBooks: books));
+      },
+    );
+  }
 }
